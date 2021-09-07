7026692aee30c4d9ea427398dfa358abced3471d
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-07 12:44:32 +0200

    Version 1.15

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.android.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /** Displays the given score for Team A. */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
