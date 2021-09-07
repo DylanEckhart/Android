635d7ef0813edd09768fffa88e772263daccd643
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-07 13:26:53 +0200

    Version 1.17

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -9,12 +9,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(5);
     }
 
     /** Displays the given score for Team A. */
@@ -40,6 +40,32 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
+
+    /** Displays the given score for Team B. */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /** Increase the score for Team B by 3 points. */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /** Increase the score for Team B by 2 points. */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /** Increase the score for Team B by 1 point. */
+    public void addOneForTeamB(View v) {
+        scoreTeamA = scoreTeamB + 1;
+        displayForTeamA(scoreTeamB);
+    }
+
+
 }
 
 
