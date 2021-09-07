89db4d7be3a65b04f4dbfe8054528c39f5ee7f64
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-07 11:11:35 +0200

    Version 1.14

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -33,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     /** This method is called when the order button is clicked. */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank you!";
         displayMessage(priceMessage);
     }
 
