9e8cdf4bc1c253412cffd9798f3260bfb2cd7eb0
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-10 11:17:54 +0200

    Version 1.3: Changed the TextBoxes into RadioButtons/RadioGroups in activity_main.xml

--- OnderwijsQuizapp/app/src/main/java/com/example/android/onderwijsquizapp/MainActivity.java
+++ OnderwijsQuizapp/app/src/main/java/com/example/android/onderwijsquizapp/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.android.onderwijsquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.Locale;
 
@@ -13,10 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Call functions
-        controlleerAntwoord1();
-        controlleerAntwoord2();
     }
 
     /** Vraag 1 */
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     String antwoordVraag2;
     boolean isAntwoordVanVraag2Correct = false;
 
-    // Check if Vraag 1 is answered correctly
+    // Check if Vraag 2 is answered correctly
     public boolean controlleerAntwoord2() {
         antwoordVraag2 = "HyperText Markup Language";
         EditText vraag2 = findViewById(R.id.insert_vraag_2);
@@ -50,7 +50,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Vraag 3 */
+    int gegevenAntwoordVraag3;
+    int correcteIDVraag3;
+    boolean isAntwoordVanVraag3Correct = false;
 
+    // Check if Vraag 3 is answered correctly
+    public int controlleerAntwoord3() {
+        RadioGroup vraag3_antwoorden = findViewById(R.id.vraag3_antwoorden);
+        gegevenAntwoordVraag3 = vraag3_antwoorden.getCheckedRadioButtonId();
+        RadioButton correcteAntwoordVraag3 = findViewById(R.id.vraag3_antwoord1);
+        correcteIDVraag3 = correcteAntwoordVraag3.getId();
 
+        isAntwoordVanVraag3Correct = gegevenAntwoordVraag3 == correcteIDVraag3;
+        return gegevenAntwoordVraag3;
+    }
+
+    /** Vraag 4 */
+    int gegevenAntwoordVraag4;
+    int correcteIDVraag4;
+    boolean isAntwoordVanVraag4Correct = false;
+
+    // Check if Vraag 4 is answered correctly
+    public int controlleerAntwoord4() {
+        RadioGroup vraag4_antwoorden = findViewById(R.id.vraag4_antwoorden);
+        gegevenAntwoordVraag4 = vraag4_antwoorden.getCheckedRadioButtonId();
+        RadioButton correcteAntwoordVraag4 = findViewById(R.id.vraag4_antwoord1);
+        correcteIDVraag4 = correcteAntwoordVraag4.getId();
+
+        isAntwoordVanVraag4Correct = gegevenAntwoordVraag4 == correcteIDVraag4;
+        return gegevenAntwoordVraag4;
+    }
+
+    public void submitAnswers(View view) {
+        controlleerAntwoord1();
+        controlleerAntwoord2();
+        controlleerAntwoord3();
+        controlleerAntwoord4();
+    }
 
 }
