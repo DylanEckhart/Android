401dd0575f0140f1ad8700111efa623c9f401eb6
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-10 09:49:32 +0200

    Version 1.2: Made functions controlleerAntwoord1 and controlleerAntwoord2 in MainActivity.java

--- OnderwijsQuizapp/app/src/main/java/com/example/android/onderwijsquizapp/MainActivity.java
+++ OnderwijsQuizapp/app/src/main/java/com/example/android/onderwijsquizapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android.onderwijsquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Call functions
+        controlleerAntwoord1();
+        controlleerAntwoord2();
     }
+
+    /** Vraag 1 */
+    String gegevenAntwoordVraag1;
+    String antwoordVraag1;
+    boolean isAntwoordVanVraag1Correct = false;
+
+    // Check if Vraag 1 is answered correctly
+    public boolean controlleerAntwoord1() {
+        antwoordVraag1 = "Tim Berners-Lee";
+        EditText vraag1 = findViewById(R.id.insert_vraag_1);
+        gegevenAntwoordVraag1 = vraag1.getText().toString().toLowerCase(Locale.ROOT);
+
+        isAntwoordVanVraag1Correct = gegevenAntwoordVraag1.equals(antwoordVraag1);
+        return isAntwoordVanVraag1Correct;
+    }
+
+    /** Vraag 2 */
+    String gegevenAntwoordVraag2;
+    String antwoordVraag2;
+    boolean isAntwoordVanVraag2Correct = false;
+
+    // Check if Vraag 1 is answered correctly
+    public boolean controlleerAntwoord2() {
+        antwoordVraag2 = "HyperText Markup Language";
+        EditText vraag2 = findViewById(R.id.insert_vraag_2);
+        gegevenAntwoordVraag2 = vraag2.getText().toString().toLowerCase(Locale.ROOT);
+
+        isAntwoordVanVraag2Correct = gegevenAntwoordVraag2.equals(antwoordVraag2);
+        return isAntwoordVanVraag2Correct;
+    }
+
+    /** Vraag 3 */
+
+
+
 }
