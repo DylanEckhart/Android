0a31621f5d19340d01148b79c2ac20ebfed77c97
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-13 14:46:00 +0200

    Made NumbersClickListener.java and intented it to MainActivity.java

--- Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.miwok;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Intent for NumbersClickListener.java
+        NumbersClickListener clickListener = new NumbersClickListener();
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        numbers.setOnClickListener(clickListener);
     }
+
 }
