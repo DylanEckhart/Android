6974a5f8e5b2afe1f0377ae57a0fe9a496aeae03
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-14 09:47:33 +0200

    Made NumbersClickListener.java and intented it to MainActivity.java

--- Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Array for the English words
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
