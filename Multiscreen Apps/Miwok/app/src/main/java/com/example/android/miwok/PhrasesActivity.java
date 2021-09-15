1744339d091fd703a12e73ff871c7d55fe8a5b37
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-15 09:29:59 +0200

    Made NumbersClickListener.java and intented it to MainActivity.java

--- Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -16,16 +16,16 @@ public class PhrasesActivity extends AppCompatActivity {
 
         // Array for the English words
         ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("Where are you going?", "minto wuksus"));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә"));
-        words.add(new Word("My name is...", "oyaaset..."));
-        words.add(new Word("How are you feeling?", "michәksәs?"));
-        words.add(new Word("I'm feeling good.", "kuchi achit"));
-        words.add(new Word("Are you coming?", "әәnәs'aa?"));
-        words.add(new Word("Yes, I'm coming.", "hәә’ әәnәm"));
-        words.add(new Word("I'm coming.", "әәnәm"));
-        words.add(new Word("Let's go.", "yoowutis"));
-        words.add(new Word("Come here.", "әnni'nem"));
+        words.add(new Word("Where are you going?", "minto wuksus", R.drawable.family_father));
+        words.add(new Word("What is your name?", "tinnә oyaase'nә", R.drawable.family_father));
+        words.add(new Word("My name is...", "oyaaset...", R.drawable.family_father));
+        words.add(new Word("How are you feeling?", "michәksәs?", R.drawable.family_father));
+        words.add(new Word("I'm feeling good.", "kuchi achit", R.drawable.family_father));
+        words.add(new Word("Are you coming?", "әәnәs'aa?", R.drawable.family_father));
+        words.add(new Word("Yes, I'm coming.", "hәә’ әәnәm", R.drawable.family_father));
+        words.add(new Word("I'm coming.", "әәnәm", R.drawable.family_father));
+        words.add(new Word("Let's go.", "yoowutis", R.drawable.family_father));
+        words.add(new Word("Come here.", "әnni'nem", R.drawable.family_father));
 
         WordAdapter adapter = new WordAdapter(this, words);
         ListView listView = findViewById(R.id.list);
--- Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -1,33 +1,39 @@
 package com.example.android.miwok;
 
 public class Word {
-    /**
-     * Default translation for the word
-     */
+    /** Default translation for the word*/
     private String mDefaultTranslation;
 
-    /**
-     * Miwok translation for the word
-     */
+    /** Miwok translation for the word*/
     private String mMiwokTranslation;
+
+    /** Image resource ID for the word.*/
+    private int mImageResourceId;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
-    /**
-     * Get the default translation of the word.
-     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
+    /** Get the default translation of the word.*/
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
-    /**
-     * Get the Miwok translation of the word.
-     */
+    /** Get the Miwok translation of the word.*/
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    /** Return the image resource ID.*/
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 }
 
