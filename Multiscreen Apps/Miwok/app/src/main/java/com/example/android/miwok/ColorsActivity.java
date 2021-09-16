3802c61727a87619ee06befcf3b388853cda1fd4
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-16 10:06:26 +0200

    Done with lesson 4

--- Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,12 +2,18 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +22,26 @@ public class ColorsActivity extends AppCompatActivity {
 
         // Array for the English words
         ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("red", "wetetti", R.drawable.color_red));
-        words.add(new Word("green", "chokokki", R.drawable.color_green));
-        words.add(new Word("brown", "takaakki", R.drawable.color_brown));
-        words.add(new Word("gray", "topoppi", R.drawable.color_gray));
-        words.add(new Word("black", "kululli", R.drawable.color_black));
-        words.add(new Word("white", "kelelli", R.drawable.color_white));
-        words.add(new Word("dusty yellow", "topiise", R.drawable.color_dusty_yellow));
-        words.add(new Word("mustard yellow", "chiwiite", R.drawable.color_mustard_yellow));
+        words.add(new Word("red", "wetetti", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("brown", "takaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("gray", "topoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("dusty yellow", "topiise", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "chiwiite", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
 
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Word word = words.get(position);
+                mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceId());
+                mMediaPlayer.start();
+            }
+        });
     }
 }
--- Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ Multiscreen Apps/Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -1,47 +1,61 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
+
 public class Word {
-    /** Default translation for the word*/
+
+    /** Default translation for the word */
     private String mDefaultTranslation;
 
-    /** Miwok translation for the word*/
+    /** Miwok translation for the word */
     private String mMiwokTranslation;
 
-    /** Image resource ID for the word.*/
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
+
+    /** Image resource ID for the word */
     private int mImageResourceId = NO_IMAGE_PROVIDED;
 
+    /* Constant value that represents no image was provided for this word. */
     private static final int NO_IMAGE_PROVIDED = -1;
 
-
-
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,
+                int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
-    /** Get the default translation of the word.*/
+    /* Get the default translation of the word.*/
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
-    /** Get the Miwok translation of the word.*/
+    /* Get the Miwok translation of the word.*/
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
 
-    /** Return the image resource ID.*/
+    /* Return the image resource ID of the word.*/
     public int getImageResourceId() {
         return mImageResourceId;
     }
 
+    /* Returns whether or not there is an image for this word.*/
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    /* Return the audio resource ID of the word.*/
+    public int getAudioResourceId() {
+        return mAudioResourceId;
     }
 }
 
