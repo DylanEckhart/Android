89007c7b02bcb8b8c08d6f6ba16968654d85c1d2
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-17 09:58:44 +0200

    Added the content to use activity_main.xml and find the View for every category

--- Multiscreen Apps/TourGuideApp/app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ Multiscreen Apps/TourGuideApp/app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android.tourguideapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,56 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
+        /* Rijksmuseum */
+        // Find the View that shows the Rijksmuseum category
+        TextView rijksmuseum = findViewById(R.id.rijksmuseum);
 
+        // Set a click listener on that View
+        rijksmuseum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rijksmuseumIntent = new Intent(MainActivity.this, RijksmuseumActivity.class);
+                startActivity(rijksmuseumIntent);
+            }
+        });
+
+        /* Anne Frank Huis */
+        // Find the View that shows the Anne Frank Huis category
+        TextView anneFrankHuis = findViewById(R.id.anne_frank_huis);
+
+        // Set a click listener on that View
+        anneFrankHuis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent anneFrankHuisIntent = new Intent(MainActivity.this, AnneFrankHuisActivity.class);
+                startActivity(anneFrankHuisIntent);
+            }
+        });
+
+        /* Vondelpark */
+        // Find the View that shows the Vondelpark category
+        TextView vondelpark = findViewById(R.id.vondelpark);
+
+        // Set a click listener on that View
+        vondelpark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vondelparkIntent = new Intent(MainActivity.this, VondelparkActivity.class);
+                startActivity(vondelparkIntent);
+            }
+        });
+
+        /* Nemo */
+        // Find the View that shows the Nemo category
+        TextView nemo = findViewById(R.id.nemo);
+
+        // Set a click listener on that View
+        nemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nemoIntent = new Intent(MainActivity.this, NemoActivity.class);
+                startActivity(nemoIntent);
+            }
+        });
     }
 }
