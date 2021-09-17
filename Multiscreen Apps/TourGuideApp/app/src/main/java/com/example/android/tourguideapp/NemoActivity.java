1579f26af0d4e6bb20269b2f12092152ac5bb6db
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-17 09:32:29 +0200

    Made the Tour Guide App project

--- Multiscreen Apps/TourGuideApp/app/src/main/java/com/example/android/tourguideapp/NemoActivity.java
+++ Multiscreen Apps/TourGuideApp/app/src/main/java/com/example/android/tourguideapp/NemoActivity.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NemoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.list_item);
     }
 }
